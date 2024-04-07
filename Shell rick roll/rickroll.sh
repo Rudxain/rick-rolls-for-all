5e23d470a724b2a23d0ecda1f7e1010db498fd5e
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-04-07 01:57:50 -0400

    make shell fully cross-platform (POSIX)

--- Shell rick roll/rickroll.sh
+++ Shell rick roll/rickroll.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-echo "starting the script"
+#!/bin/sh
+
+echo "starting the script..."
 sleep 3
 echo "We're no strangers to love
 You know the rules and so do I
@@ -8,14 +9,14 @@ You wouldn't get this from any other guy"
 sleep 3
 echo "I just wanna tell you how I'm feeling
 Gotta make you understand"
-sleep 3 
+sleep 3
 echo "Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you"
-sleep 3 
+sleep 3
 echo "We've known each other for so long
 Your heart's been aching but you're too shy to say it
 Inside we both know what's been going on
@@ -29,7 +30,7 @@ Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you"
-sleep 3 
+sleep 3
 echo "Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
@@ -39,33 +40,32 @@ Never gonna tell a lie and hurt you"
 sleep 3
 echo "Never gonna give, never gonna give
 (Give you up)"
-sleep 3 
+sleep 3
 echo "We've known each other for so long
 Your heart's been aching but you're too shy to say it
 Inside we both know what's been going on
 We know the game and we're gonna play it"
-sleep 3 
+sleep 3
 echo "I just wanna tell you how I'm feeling
 Gotta make you understand"
-sleep 3 
+sleep 3
 echo "Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you"
-sleep 3 
+sleep 3
 echo "Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you"
-sleep 3 
+sleep 3
 echo "Never gonna give you up
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you"
-exit 
